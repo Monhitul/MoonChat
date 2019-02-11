f00840fd75f9555675af780f8239adcd77901605
K.B <kb163159163@163.com>
2019-02-11 19:06:44 +0800

    init

---
+++ src/main/java/com/moon/moonchat/MoonchatApplication.java
@@ -0,0 +1,14 @@
+package com.moon.moonchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoonchatApplication.class, args);
+    }
+
+}
+
